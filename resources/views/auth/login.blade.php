174db5b4b2f4faf5e825c24c792ed6c1b76889b1
tomassue <tomabella64@gmail.com>
2024-01-22 10:29:37 +0800

    UPDATE: Login front end
    > Can login with given credentials.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<div class="container-fluid d-md-flex flex-md-equal">
+    <!-- Left Div -->
+    <div class="col-md-6 left-div">
+        <!-- Your content for the left div goes here -->
+        <h1 class="text-center">Left Div</h1>
+    </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <!-- Right Div -->
+    <div class="col-md-6 right-div" style="background-color: #FFFF;">
+        <div class="form-container" style="background-color: #FFFF; color: white;">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="mt-5 mb-5 py-5 px-5 text-center" style="display: block; width: auto;">
+                <img src="{{ asset('assets/img/cdo-seal.png') }}" alt="cdo-seal" style="height: 185px; width: 180px;">
+                <img src="{{ asset('assets/img/rise.png') }}" alt="cdo-seal" style="height: 135px; width: 250px;">
             </div>
+
+            <form method="POST" action="{{ route('login') }}">
+                <!-- You need @csrf when using POST as the form method -->
+                @csrf
+
+
+                <div class="mb-1 row">
+                    <div class="d-flex justify-content-center mb-2">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="USERNAME" aria-label="default input example" style="width: 400px; text-align: center;" required autocomplete="email" autofocus>
+                    </div>
+
+                    <div class="d-flex justify-content-center mb-2">
+                        @error('email')
+                        <span class="badge border-danger border-1 text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required automcomplete="current-password" placeholder="PASSWORD" aria-label="default input example" style="width: 400px; text-align: center;">
+                    </div>
+
+                    <div class="d-flex justify-content-center mb-2">
+                        @error('password')
+                        <span class="badge border-danger border-1 text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-center pt-3">
+                    <button type="submit" class="btn btn-primary fs-w" style="width: 350px;"><b>LOGIN</b></button>
+                </div>
+
+            </form>
         </div>
     </div>
 </div>
+
 @endsection
