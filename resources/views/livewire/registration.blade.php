bcd2e90c5777ee0ee7d406d0a0355bd5dd6f8f41
Tom Abella <tomabella64@gmail.com>
2024-02-07 18:43:19 +0800

    :bug: FIXED
    > Search for Registration ORG were not working.

--- resources/views/livewire/registration.blade.php
+++ resources/views/livewire/registration.blade.php
@@ -86,7 +86,7 @@
             <div class="row mx-5 mt-4 mb-4">
                 <div class="input-group mb-4">
                     <span class="input-group-text fw-bolder fs-4" id="basic-addon1"><i class="bi bi-search"></i></span>
-                    <input type="text" class="form-control form-control-lg" aria-label="Search" aria-describedby="basic-addon1" placeholder="Rider" wire:model.live.debounce.300ms="search_one">
+                    <input type="text" class="form-control form-control-lg" aria-label="Search" aria-describedby="basic-addon1" placeholder="Rider" wire:model.live.debounce.300ms="search_one_org">
                 </div>
                 @if($noRecordsRegisteredMembers)
                 <div class="pagination-info pt-4">
