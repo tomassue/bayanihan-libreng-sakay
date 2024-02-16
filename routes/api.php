70854fa3ae9c497728d316ac48b973110769873d
Tom Abella <tomabella64@gmail.com>
2024-02-17 01:44:28 +0800

    ONGOING api
    > Registration
    > Login (check session)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/register', [RegistrationController::class, 'register']); // Not sure 'bout this.
