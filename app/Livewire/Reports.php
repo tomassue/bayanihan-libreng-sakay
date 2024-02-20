513e4390cf75cf0c8644e70f10046caa4924e121
tomassue <tomabella64@gmail.com>
2024-02-20 16:56:13 +0800

    ONGOING Client List > QR Code generate
    > Added dompdf

--- app/Livewire/Reports.php
+++ app/Livewire/Reports.php
@@ -2,17 +2,46 @@
 
 namespace App\Livewire;
 
+use App\Models\ClientInformationModel;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
+use Livewire\WithPagination;
+use BaconQrCode\Encoder\QrCode;
 
 #[Layout('components.layouts.page')]
-#[Title('Dashboard')]
+#[Title('Reports')]
 
 class Reports extends Component
 {
+    use WithPagination;
+
+    // Function encrypt($text_data)
+    private string $encryptMethod = 'AES-256-CBC';
+    private string $key;
+    private string $iv;
+
     public function render()
     {
-        return view('livewire.reports');
+        $clients = ClientInformationModel::paginate(10, pageName: 'list-of-clients');
+
+        return view('livewire.reports', [
+            'clients'                   =>      $clients,
+            'currentPageclients'        =>      $clients->currentPage(),
+            'totalPagesclients'         =>      $clients->lastPage(),
+            'totalRecordsclients'       =>      $clients->total(),
+            'noRecordsclients'          =>      $clients->isEmpty(),
+        ]);
+    }
+
+    private function encrypt($text_data): string
+    {
+        $mykey = 'CagayanDeOroIctQrCode';
+        $myiv = 'ThisIsASecuredBlock';
+        $this->key = substr(hash('sha256', $mykey), 0, 32);
+        $this->iv = substr(hash('sha256', $myiv), 0, 16);
+        $ciphertext_raw = openssl_encrypt($text_data, $this->encryptMethod, $this->key, 0, $this->iv);
+
+        return $ciphertext_raw;
     }
 }
--- resources/views/livewire/reports.blade.php
+++ resources/views/livewire/reports.blade.php
@@ -1,3 +1,72 @@
 <div>
-    <h1>wew</h1>
+    <div>
+        <style>
+            .custom-border {
+                border: 1px solid #dee2e6;
+                /* Set your preferred border color here */
+                border-radius: 0.25rem;
+                /* Optional: Add border radius for a rounded appearance */
+            }
+
+            .custom-border thead th,
+            .custom-border tbody tr:last-child td {
+                border-bottom: none;
+                /* Remove border for the last row in tbody */
+            }
+        </style>
+
+        <div class="col-12">
+            <div class="card border border-secondary" wire:loading.class="opacity-50" wire:target="pageOne, pageTwo, pageThree">
+                <div class="row mx-5 mt-4">
+
+                    @if(session('status'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <i class="bi bi-check-circle me-1"></i>
+                        {{ session('status') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
+
+                </div>
+
+                <div class="row mx-5 mt-4 mb-4">
+                    <div class="col text-center table-responsive">
+                        <div class="text-start" style="color: #0A335D;">
+                            <h1>Clients</h1>
+                        </div>
+                        <div class="pagination-info pb-2 text-start">
+                            Page {{ $currentPageclients }} out of {{ $totalPagesclients }}, Total Records: {{ $totalRecordsclients }}
+                        </div>
+                        <table class="table table-borderless">
+                            <thead>
+                                <tr>
+                                    <th scope="col">CLIENT'S NAME</th>
+                                    <th scope="col">ADDRESS</th>
+                                    <th scope="col">CONTACT NUMBER</th>
+                                    <th scope="col">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="text-start" style="border-top: 1px solid black; border-right: 1px solid black; border-left: 1px solid black; border-bottom: 1px solid black; border-radius: 10px;">
+                                    <td colspan="5" style="background-image: linear-gradient(#2E8B57 53%, #0A335D 100%);"><span style="font-size:larger; font-weight:bolder; color:#FFFFFF">LIST</span></td>
+                                </tr>
+                                @foreach($clients as $client)
+                                <tr style="border-right: 1px solid black; border-left: 1px solid black; border-bottom: 1px solid black;">
+                                    <th scope="row">{{ $client->first_name }}</th>
+                                    <td>{{ $client->address }}</td>
+                                    <td>{{ $client->contact_number }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success fw-bold mx-auto" style="width: 100px; height: 30px; padding-right: 0px; padding-top: 0px; padding-left: 0px; padding-bottom: 0px;">GENERATE</button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{ $clients->links('vendor.livewire.custom-pagination') }}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
