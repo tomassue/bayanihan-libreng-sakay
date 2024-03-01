ada4b2e8be3f460a3adf7bd7cc90d9c2f057405c
tomassue <tomabella64@gmail.com>
2024-03-01 16:11:11 +0800

    Update Offline.php
    > Text changes.

--- app/Livewire/Offline.php
+++ app/Livewire/Offline.php
@@ -11,7 +11,7 @@ class Offline extends Component
         return <<<'HTML'
         <div wire:offline>
             <li class="nav-item dropdown pe-3">
-                <span class="badge bg-danger">Whoops, your device has lost connection. The web page you are viewing is offline.</span>
+                <span class="badge bg-danger">You're offline.</span>
             </li>
         </div>
         HTML;
